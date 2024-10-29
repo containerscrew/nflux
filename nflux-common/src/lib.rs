9df936626708f1d7e277fe4de23b60491c88a5a7
containerscrew <containerscrew@users.noreply.github.com>
2024-10-29 23:51:20 +0100

    chore: Using perfbuff

--- nflux-common/src/lib.rs
+++ nflux-common/src/lib.rs
@@ -39,3 +39,13 @@ pub const MAX_FIREWALL_RULES: u32 = 32;
 pub const MAX_RULES_PORT: usize = 32;
 pub const MAX_ALLOWED_PORTS: usize = 1024;
 pub const MAX_ALLOWED_IPV4: usize = 1024;
+
+#[repr(C)]
+#[derive(Debug)]
+pub struct ConnectionEvent {
+    pub src_addr: u32,
+    pub dst_addr: u32,
+    pub src_port: u16,
+    pub dst_port: u16,
+    pub protocol: u8, // 6 for TCP, 17 for UDP
+}
