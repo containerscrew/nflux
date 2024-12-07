142a5ef6ca8b316126d5f0d08b332182f3b1581d
containerscrew <containerscrew@users.noreply.github.com>
2024-12-07 14:04:20 +0100

    chore: Working with TC egress implementation

---
+++ nflux-ebpf/src/egress.rs
@@ -0,0 +1,36 @@
+use aya_ebpf::bindings::TC_ACT_PIPE;
+use aya_ebpf::macros::map;
+use aya_ebpf::maps::{LruHashMap, PerfEventArray};
+use aya_ebpf::programs::TcContext;
+use network_types::eth::{EthHdr, EtherType};
+use network_types::ip::Ipv4Hdr;
+use nflux_common::EgressEvent;
+
+#[map]
+static ACTIVE_CONNECTIONS: LruHashMap<u32, u32> = LruHashMap::with_max_entries(1024, 0);
+
+#[map]
+static EGRESS_EVENT: PerfEventArray<EgressEvent> = PerfEventArray::new(0);
+
+pub fn try_tc_egress(ctx: TcContext) -> Result<i32, ()> {
+    let ethhdr: EthHdr = ctx.load(0).map_err(|_| ())?;
+    match ethhdr.ether_type {
+        EtherType::Ipv4 => unsafe {
+            let ipv4hdr: Ipv4Hdr = ctx.load(EthHdr::LEN).map_err(|_| ())?;
+            let destination = u32::from_be(ipv4hdr.dst_addr);
+
+            // Check if this destination is already active
+            if ACTIVE_CONNECTIONS.get(&destination).is_none() {
+                // Log only new connections
+                let event = EgressEvent { dst_ip: destination };
+                EGRESS_EVENT.output(&ctx, &event, 0);
+
+                // Mark connection as active
+                ACTIVE_CONNECTIONS.insert(&destination, &1, 0).map_err(|_| ())?;
+            }
+        }
+        _ => return Ok(TC_ACT_PIPE),
+    }
+
+    Ok(TC_ACT_PIPE)
+}
--- nflux/src/utils.rs
+++ nflux/src/utils.rs
@@ -1,3 +1,5 @@
+use std::net::{IpAddr, Ipv4Addr};
+use dns_lookup::lookup_addr;
 use libc::getuid;
 use tokio::signal;
 use tracing::{info, warn};
@@ -13,4 +15,15 @@ pub async fn wait_for_shutdown() -> anyhow::Result<()> {
     ctrl_c.await?;
     warn!("Exiting...");
     Ok(())
+}
+
+pub fn lookup_address(ip: u32) -> String {
+    // Convert the u32 IP address to Ipv4Addr
+    let ip = Ipv4Addr::from(ip);
+
+    // Convert to IpAddr for compatibility with lookup_addr
+    let ip = IpAddr::V4(ip);
+
+    // Perform the reverse DNS lookup
+    lookup_addr(&ip).unwrap_or_else(|_| "Unknown host".to_string())
 }
