a40c78424eef18486894807ce0987a2949bb5dfb
containerscrew <containerscrew@users.noreply.github.com>
2024-12-18 13:29:06 +0100

    chore: Refactor ipv4 traffic

--- nflux/src/logger.rs
+++ nflux/src/logger.rs
@@ -1,27 +1,29 @@
 use tracing_subscriber::fmt::format::FmtSpan;
+use tracing::Level;
 
-pub fn setup_logger(log_level: &String, format: &String) {
-    let log_level = match log_level.as_str() {
-        "trace" => tracing::Level::TRACE,
-        "debug" => tracing::Level::DEBUG,
-        "info" => tracing::Level::INFO,
-        "warn" => tracing::Level::WARN,
-        "error" => tracing::Level::ERROR,
-        _ => tracing::Level::INFO,
+pub fn setup_logger(log_level: &str, format: &str) {
+    // Match the log level
+    let log_level = match log_level {
+        "trace" => Level::TRACE,
+        "debug" => Level::DEBUG,
+        "info" => Level::INFO,
+        "warn" => Level::WARN,
+        "error" => Level::ERROR,
+        _ => Level::INFO,
     };
 
-    // Create a base subscriber with common settings
+    // Create a subscriber with stdout as the target
     let base_subscriber = tracing_subscriber::fmt()
-        .with_thread_names(false)
         .with_max_level(log_level)
+        .with_thread_names(false)
         .with_span_events(FmtSpan::FULL)
         .with_file(false)
         .with_target(false);
 
-    // Apply JSON or text formatting based on the format parameter
-    match format.as_str() {
-        "json" => base_subscriber.json().init(),
-        "text" => base_subscriber.init(),
-        _ => base_subscriber.init(),
-    };
+    // JSON or text format (default to JSON for container environments)
+    if format == "json" {
+        base_subscriber.json().init();
+    } else {
+        base_subscriber.init();
+    }
 }
