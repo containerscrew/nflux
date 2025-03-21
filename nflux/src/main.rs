bac2eefb25c0fb0710881abde57d279ec3d9c864
containerscrew <containerscrew@users.noreply.github.com>
2025-03-21 12:51:51 +0100

    chore: Restructure cli code

--- nflux/src/main.rs
+++ nflux/src/main.rs
@@ -1,9 +1,7 @@
 use std::process::{self, exit};
 
 use aya::{include_bytes_aligned, maps::RingBuf, Ebpf};
-use clap::Parser;
-use cli::Cli;
-use custom_logger::{setup_logger, LogFormat};
+use custom_logger::setup_logger;
 use libc::getuid;
 use nflux_common::Configmap;
 use tc_event::process_event;
@@ -22,61 +20,46 @@ mod utils;
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
     // Start CLI
-    let cli = Cli::parse();
-
-    // Enable logging
-    let log_format = if cli.log_format == "json" {
-        LogFormat::Json
-    } else {
-        LogFormat::Text
-    };
-    setup_logger(&cli.log_level, log_format);
-
-    // Ensure the program is run as root
-    let uid = unsafe { getuid() };
-    if let Err(e) = check_is_root_user(uid) {
-        error!("{}", e);
-        exit(1);
+    match cli::start_cli() {
+        Ok(_) => {
+        } // Do nothing
+        Err(err) => {
+            error!("Error starting the cli {}", err)
+        }
     }
 
-    // Welcome message
-    info!("Starting nflux with pid {}", process::id());
+    // // Load eBPF program
+    // let mut bpf = Ebpf::load(include_bytes_aligned!(concat!(env!("OUT_DIR"), "/nflux")))?;
 
-    // Set memory limit.
-    set_mem_limit();
+    // // Traffic control configuration. This data will be used in a shared ebpf map
+    // let configmap = Configmap {
+    //     disable_private_ips: 1, // Not implemented yet
+    //     enable_udp: is_true(cli.enable_udp),                   // 0 = no, 1 = yes
+    //     enable_icmp: is_true(cli.enable_icmp),
+    //     enable_tcp: is_true(cli.enable_tcp),
+    // };
 
-    // Load eBPF program
-    let mut bpf = Ebpf::load(include_bytes_aligned!(concat!(env!("OUT_DIR"), "/nflux")))?;
+    // // Attach TC program to the interface
+    // try_traffic_control(
+    //     &mut bpf,
+    //     cli.interface,
+    //     cli.enable_ingress,
+    //     cli.enable_egress,
+    //     configmap,
+    // )?;
 
-    // Traffic control configuration. This data will be used in a shared ebpf map
-    let configmap = Configmap {
-        disable_private_ips: 1, // Not implemented yet
-        enable_udp: is_true(cli.enable_udp),                   // 0 = no, 1 = yes
-        enable_icmp: is_true(cli.enable_icmp),
-        enable_tcp: is_true(cli.enable_tcp),
-    };
+    // // Traffic control event ring buffer
+    // let tc_event_ring_map = bpf
+    //     .take_map("TC_EVENT")
+    //     .ok_or_else(|| anyhow::anyhow!("Failed to find ring buffer TC_EVENT map"))?;
 
-    // Attach TC program to the interface
-    try_traffic_control(
-        &mut bpf,
-        cli.interface,
-        cli.enable_ingress,
-        cli.enable_egress,
-        configmap,
-    )?;
+    // let ring_buf = RingBuf::try_from(tc_event_ring_map)?;
 
-    // Traffic control event ring buffer
-    let tc_event_ring_map = bpf
-        .take_map("TC_EVENT")
-        .ok_or_else(|| anyhow::anyhow!("Failed to find ring buffer TC_EVENT map"))?;
+    // // Spawn a task to process events
+    // tokio::spawn(async move { process_event(ring_buf).await });
 
-    let ring_buf = RingBuf::try_from(tc_event_ring_map)?;
-
-    // Spawn a task to process events
-    tokio::spawn(async move { process_event(ring_buf).await });
-
-    // Wait for shutdown
-    let _ = wait_for_shutdown().await;
+    // // Wait for shutdown
+    // let _ = wait_for_shutdown().await;
 
     Ok(())
 }
