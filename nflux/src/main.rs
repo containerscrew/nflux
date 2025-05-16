bef8d26e5cf7fccbc306d1832e167998ac1570fb
containerscrew <containerscrew@users.noreply.github.com>
2025-05-16 16:20:51 +0200

    Refactor nflux/ package

--- nflux/src/main.rs
+++ nflux/src/main.rs
@@ -1,20 +1,65 @@
-use tracing::error;
+use std::process;
+use std::process::exit;
+use clap::Parser;
+use libc::getuid;
+use nflux_common::Configmap;
+use tracing::{error, info};
+use try_nflux::start_nflux;
+use utils::{is_true, set_mem_limit};
+use crate::cli::NfluxCliArgs;
+use crate::logger::init_logger;
+use crate::utils::check_is_root_user;
 
-mod cli;
 mod logger;
 mod tc_event;
-mod try_netrace;
+mod try_nflux;
 mod utils;
 
+mod cli;
+
 #[tokio::main]
-async fn main() -> anyhow::Result<()> {
-    // Start CLI
-    match cli::start_cli().await {
-        Ok(_) => {} // Do nothing
-        Err(err) => {
-            error!("Error starting the cli {}", err)
-        }
+async fn main() {
+    // Parse cli args
+    let cli = NfluxCliArgs::parse();
+
+    // Start logger
+    init_logger(&cli.log_level, &cli.log_format.as_str());
+
+    // User should be root
+    let uid = unsafe { getuid() };
+    if let Err(e) = check_is_root_user(uid) {
+        error!("{}", e);
+        exit(1);
     }
 
-    Ok(())
+    // Set memory limit for eBPF maps
+    set_mem_limit();
+
+    // Welcome message
+    info!("Starting nflux with pid {}", process::id());
+
+    // If enable_egress and enable_ingress are both false, the app is doing nothing, exit
+    if cli.disable_egress && cli.disable_ingress {
+        error!("Can't disable both egress and ingress traffic, nothing to display :)");
+        exit(1)
+    }
+
+    // Also, if all protocols are disabled, exit
+    if cli.disable_icmp && cli.disable_tcp && cli.disable_udp {
+        error!("You disabled all the protocols (tcp/udp/icmp), nothing to display :)");
+        exit(1)
+    }
+
+    // Prepare configmap data (data from user space to be processed in kernel space using eBPF maps)
+    let configmap = Configmap {
+        disable_private_ips: 1,             // Not implemented yet
+        disable_udp: is_true(cli.disable_udp), // 0 = no, 1 = yes
+        disable_icmp: is_true(cli.disable_icmp),
+        disable_tcp: is_true(cli.disable_tcp),
+        log_interval: cli.log_interval,
+        disable_full_log: is_true(cli.disable_full_log),
+    };
+
+    // Start nflux
+    start_nflux(&cli.interface, cli.disable_egress, cli.disable_ingress, configmap).await.expect("Failed to start nflux");
 }
--- nflux/src/utils.rs
+++ nflux/src/utils.rs
@@ -88,7 +88,7 @@ pub fn convert_protocol(protocol: u8) -> &'static str {
     }
 }
 
-pub fn get_service_name(port: u16, proto: &'static str) -> String {
+pub fn _get_service_name(port: u16, proto: &'static str) -> String {
     let c_proto = CString::new(proto).unwrap_or_else(|_| CString::new("").unwrap());
     let c_port = ntohs(port);
 
@@ -141,5 +141,11 @@ mod tests {
             result.unwrap_err(),
             "This program must be run as root. Try: $ sudo nflux -i iface-name"
         );
+    }
+
+    #[test]
+    fn test_is_true() {
+        assert_eq!(is_true(true), 1);
+        assert_eq!(is_true(false), 0);
     }
 }
