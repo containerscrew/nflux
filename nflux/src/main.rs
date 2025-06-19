f13d17a592315bc023b23ac5d537afff2acd850f
containerscrew <containerscrew@users.noreply.github.com>
2025-06-19 14:59:23 +0200

    refactor: rename some functions & delete comments in nflux/src/main.rs

--- nflux/src/main.rs
+++ nflux/src/main.rs
@@ -8,7 +8,7 @@ use tracing::{error, info};
 use try_nflux::start_nflux;
 use utils::{is_true, set_mem_limit};
 
-use crate::{cli::NfluxCliArgs, logger::init_logger, utils::check_is_root_user};
+use crate::{cli::NfluxCliArgs, logger::init_logger, utils::is_root_user};
 
 mod logger;
 mod tc_event;
@@ -18,20 +18,17 @@ mod utils;
 mod cli;
 
 #[tokio::main]
-async fn main() {
-    // Parse cli args
+async fn main() -> anyhow::Result<()> {
     let cli = NfluxCliArgs::parse();
 
-    // Start logger
     init_logger(LoggerConfig {
         level: cli.log_level,
         format: cli.log_format.clone(),
         with_timer: cli.with_timer,
     });
 
-    // User should be root
     let uid = unsafe { getuid() };
-    if let Err(e) = check_is_root_user(uid) {
+    if let Err(e) = is_root_user(uid) {
         error!("{}", e);
         exit(1);
     }
@@ -39,7 +36,6 @@ async fn main() {
     // Set memory limit for eBPF maps
     set_mem_limit();
 
-    // Welcome message
     info!("Starting nflux with pid {}", process::id());
 
     // If enable_egress and enable_ingress are both false, the app is doing nothing, exit
@@ -56,7 +52,6 @@ async fn main() {
 
     // Prepare configmap data (data from user space to be processed in kernel space using eBPF maps)
     let configmap = Configmap {
-        disable_private_ips: 1,                // Not implemented yet
         disable_udp: is_true(cli.disable_udp), // 0 = no, 1 = yes
         disable_icmp: is_true(cli.disable_icmp),
         disable_tcp: is_true(cli.disable_tcp),
@@ -76,4 +71,5 @@ async fn main() {
     )
     .await
     .expect("Failed to start nflux");
+    Ok(())
 }
