51d746ba864a359b31fe8cd57411f7e3718a44ed
containerscrew <containerscrew@users.noreply.github.com>
2025-07-07 22:04:11 +0200

    feat: refactor to eliminate high stack allocation, reducing verifier stack usage

--- nflux-ebpf-tc/src/tc_event.rs
+++ nflux-ebpf-tc/src/tc_event.rs
@@ -1,5 +1,5 @@
 use aya_ebpf::helpers::r#gen::bpf_ktime_get_ns;
-use nflux_common::{Configmap, IpFamily, TcEvent, TcpFlags};
+use nflux_common::{IpFamily, TcEvent, TcpFlags};
 
 use crate::maps::{ActiveConnectionKey, ACTIVE_CONNECTIONS, TC_EVENT};
 
@@ -15,7 +15,8 @@ pub unsafe fn log_connection_fields(
     direction: u8,
     ip_family: IpFamily,
     tcp_flags: TcpFlags,
-    configmap: Configmap,
+    disable_full_log: u8,
+    log_interval: u64,
 ) {
     let key = ActiveConnectionKey {
         protocol,
@@ -25,13 +26,13 @@ pub unsafe fn log_connection_fields(
         dst_ip,
     };
 
-    let should_log = if configmap.disable_full_log == 0 {
+    let should_log = if disable_full_log == 0 {
         true
     } else {
         let current_time = bpf_ktime_get_ns();
 
         if let Some(last_log_time) = ACTIVE_CONNECTIONS.get(&key) {
-            if current_time - *last_log_time < configmap.log_interval {
+            if current_time - *last_log_time < log_interval {
                 return;
             }
         }
@@ -62,52 +63,3 @@ pub unsafe fn log_connection_fields(
         }
     }
 }
-
-// #[inline]
-// pub unsafe fn log_connection(
-//     event: &TcEvent,
-//     configmap: Configmap,
-// ) {
-//     // By default, we log all events
-//     if configmap.disable_full_log == 0 {
-//         if let Some(mut data) = TC_EVENT.reserve::<TcEvent>(0) {
-//             unsafe {
-//                 core::ptr::write(data.as_mut_ptr(), *event);
-//             }
-//             data.submit(0);
-//         }
-//     } else {
-//         let current_time = bpf_ktime_get_ns();
-
-//         let key = ActiveConnectionKey {
-//             protocol: event.protocol,
-//             src_port: event.src_port,
-//             dst_port: event.dst_port,
-//             src_ip: event.src_ip,
-//             dst_ip: event.dst_ip,
-//         };
-
-//         if let Some(last_log_time) = ACTIVE_CONNECTIONS.get(&key) {
-//             if current_time - *last_log_time < configmap.log_interval {
-//                 return;
-//             }
-//         }
-
-//         if let Some(mut data) = TC_EVENT.reserve::<TcEvent>(0) {
-//             unsafe {
-//                 core::ptr::write(data.as_mut_ptr(), *event);
-//             }
-//             data.submit(0);
-//         }
-
-//         ACTIVE_CONNECTIONS.insert(&key, &current_time, 0).ok();
-//     }
-// }
-
-// if let Some(mut data) = TC_EVENT.reserve::<TcEvent>(0) {
-//     unsafe {
-//         let ptr = data.as_mut_ptr();
-//         core::ptr::write(ptr, TcEvent { ... });
-//     }
-//     data.submit(0);
-// }
