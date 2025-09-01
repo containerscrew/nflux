69e49a92cd00e37420ee02eaeef804071fb1070f
containerscrew <containerscrew@users.noreply.github.com>
2025-09-01 16:22:55 +0200

    refactor: pre-commit

--- scripts/pre-commit.sh
+++ scripts/pre-commit.sh
@@ -5,9 +5,6 @@ separator() {
     echo -e "\n--- $1 ---\n"
 }
 
-separator "Formatting Rust code"
-cargo fmt --all > /dev/null
-
 separator "Running pre-commit hooks"
 pre-commit run -a --show-diff-on-failure
 
